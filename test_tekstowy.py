51e22d9dfde3bb466647832c0264121e29f23f49
Tomasz Kazimierczuk <Tomasz.Kazimierczuk@fuw.edu.pl>
2020-04-01 16:21:50 +0200

    Tekstowy test klasy Populacja

---
+++ test_tekstowy.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  1 16:18:49 2020
+
+@author: tkaz
+"""
+
+from symulacja import Pacjent,Populacja
+
+pop = Populacja(n = 7)
+print(pop)
+pop.ruch()
+print("--------------")
+print(pop)
