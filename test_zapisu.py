f3d7cf12b686f6fffc3d1e7a46a9eea1af5b8a67
Tomasz Kazimierczuk <Tomasz.Kazimierczuk@fuw.edu.pl>
2020-04-02 13:54:13 +0200

    Dodany test zapisu

---
+++ test_zapisu.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  2 13:19:09 2020
+
+@author: tkaz
+"""
+
+from symulacja import Pacjent,Populacja
+
+pop = Populacja(5)
+pop.zapisz_do_pliku('test.txt')
