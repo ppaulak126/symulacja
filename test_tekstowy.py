42802c3901bce9178bc8cf03c503027daf6581af
Tomasz Kazimierczuk <Tomasz.Kazimierczuk@fuw.edu.pl>
2020-04-02 13:21:37 +0200

    Dodany test tekstowy

---
+++ test_tekstowy.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  2 13:19:09 2020
+
+@author: tkaz
+"""
+
+from symulacja import Pacjent,Populacja
+
+pop = Populacja(5)
+print(pop)
+print("-------")
+pop.ruch()
+print(pop)
